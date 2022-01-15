c3c847032fff3326f5b61ec291b91caa98296bf1
Jeff Hutchison <jeff@jeffhutchison.com>
2022-01-14 19:07:33 -0500

    Update for Third Coast 22.0.0 telemetry

--- src/main/java/frc/robot/Registerable.java
+++
@@ -1,9 +0,0 @@
-package frc.robot;
-
-import org.strykeforce.telemetry.TelemetryService;
-
-public interface Registerable {
-
-    void registerTelemetry(TelemetryService telemetryService);
-    
-}
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,15 +1,14 @@
 package frc.robot.subsystems;
 
+import org.jetbrains.annotations.NotNull;
 import org.strykeforce.swerve.SwerveDrive;
 import org.strykeforce.swerve.TalonSwerveModule;
+import org.strykeforce.telemetry.Registrable;
 import org.strykeforce.telemetry.TelemetryService;
-
-import frc.robot.Registerable;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.RobotContainer;
 import frc.robot.Constants.DriveConstants;
 import static frc.robot.Constants.kTalonConfigTimeout;
 
@@ -17,23 +16,23 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-public class DriveSubsystem extends SubsystemBase implements Registerable {
+public class DriveSubsystem extends SubsystemBase implements Registrable {
 
     private SwerveDrive swerveDrive;
     public DriveSubsystem()
     {
-        
+
             var moduleBuilder =
                 new TalonSwerveModule.Builder()
                     .driveGearRatio(DriveConstants.kDriveGearRatio)
                     .wheelDiameterInches(DriveConstants.kWheelDiameterInches)
                     .driveMaximumMetersPerSecond(DriveConstants.kMaxSpeedMetersPerSecond);
-        
+
             TalonSwerveModule[] swerveModules = new TalonSwerveModule[4];
             Translation2d[] wheelLocations = DriveConstants.getWheelLocationMeters();
-        
-           
-        
+
+
+
             for (int i = 0; i < 4; i++) {
               var azimuthTalon = new TalonSRX(i);
               azimuthTalon.configFactoryDefault(kTalonConfigTimeout);
@@ -41,33 +40,29 @@ public class DriveSubsystem extends SubsystemBase implements Registerable {
               azimuthTalon.enableCurrentLimit(true);
               azimuthTalon.enableVoltageCompensation(true);
               azimuthTalon.setNeutralMode(NeutralMode.Coast);
-        
+
               var driveTalon = new TalonFX(i + 10);
               driveTalon.configFactoryDefault(kTalonConfigTimeout);
               driveTalon.configAllSettings(DriveConstants.getDriveTalonConfig(), kTalonConfigTimeout);
               driveTalon.enableVoltageCompensation(true);
               driveTalon.setNeutralMode(NeutralMode.Brake);
-        
+
               swerveModules[i] =
                   moduleBuilder
                       .azimuthTalon(azimuthTalon)
                       .driveTalon(driveTalon)
                       .wheelLocationMeters(wheelLocations[i])
                       .build();
-        
+
               swerveModules[i].loadAndSetAzimuthZeroReference();
-              
-              
+
+
             }
-        
+
             swerveDrive = new SwerveDrive(swerveModules);
             swerveDrive.resetGyro();
             swerveDrive.setGyroOffset(Rotation2d.fromDegrees(180));
-        
-    }
 
-    @Override
-    public void registerTelemetry(TelemetryService telemetryService) {
     }
 
     public void drive(double forwardMetersPerSec, double strafeMetersPerSec, double yawRadiansPerSec)
@@ -85,4 +80,9 @@ public class DriveSubsystem extends SubsystemBase implements Registerable {
     {
         swerveDrive.resetGyro();
     }
+
+  @Override
+  public void registerWith(@NotNull TelemetryService telemetryService) {
+   swerveDrive.registerWith(telemetryService);
+  }
 }
