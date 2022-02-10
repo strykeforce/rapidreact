ee5f10d8822c2527e5e3a053fd84b5c29de13f55
Jeff Hutchison <jeff@jeffhutchison.com>
2022-02-10 12:40:46 -0500

    Add custom Deadeye pipeline for Hub targets (#20)
    * Add custom Deadeye pipeline for Hub targets
    
    * Rename things in CMake build
    
    * Add Ansible deploy playbook
    
    * Revert local changes to submodule
    
    * Add install for deadeyed
    
    * Finish playbook steps
    
    * Add robots to inventory

---
+++ deadeye/src/hub_target_data.cpp
@@ -0,0 +1,34 @@
+#include "hub_target_data.h"
+
+#include <fmt/core.h>
+
+#include <opencv2/imgproc.hpp>
+
+using namespace deadeye;
+using namespace rr;
+using json = nlohmann::json;
+
+HubTargetData::HubTargetData(std::string id, int sn, bool valid,
+                             cv::Rect bb)
+    : TargetData{id, sn, valid}, bb(bb), center((bb.tl() + bb.br()) / 2) {}
+
+void HubTargetData::DrawMarkers(cv::Mat& preview) const {
+  cv::drawMarker(preview, center, cv::Scalar::all(255));
+  cv::rectangle(preview, bb, cv::Scalar(255, 0, 0));
+}
+
+std::string HubTargetData::Dump() const {
+  json j =
+      json{{TargetData::kIdKey, id},
+           {TargetData::kSerialKey, serial},
+           {TargetData::kValidKey, valid},
+           {TargetData::kDataKey,
+            {bb.tl().x, bb.tl().y, bb.br().x, bb.br().y, center.x, center.y}}};
+  return j.dump();
+}
+
+std::string HubTargetData::ToString() const {
+  return fmt::format("id={} sn={} val={} tl=({},{}) br=({},{}) off=({},{})", id,
+                     serial, valid, bb.tl().x, bb.tl().y, bb.br().x, bb.br().y,
+                     center.x, center.y);
+}
