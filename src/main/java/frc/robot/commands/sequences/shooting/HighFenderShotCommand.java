7975448bb2a04847100f5ce7f5aa5e67fc6115a3
kennyseybold <42521401+kennyseybold@users.noreply.github.com>
2022-03-16 21:04:51 -0400

    Magazine tested and need function for stop in shoot sequences

--- src/main/java/frc/robot/commands/sequences/shooting/HighFenderShotCommand.java
+++ src/main/java/frc/robot/commands/sequences/shooting/HighFenderShotCommand.java
@@ -2,6 +2,7 @@ package frc.robot.commands.sequences.shooting;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.MagazineSubsystem;
+import frc.robot.subsystems.MagazineSubsystem.UpperMagazineState;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.TurretSubsystem;
 
@@ -29,15 +30,15 @@ public class HighFenderShotCommand extends CommandBase {
 
   @Override
   public boolean isFinished() {
-    return magazineSubsystem.isMagazineEmpty() && !magazineSubsystem.isUpperBeamBroken();
+    return magazineSubsystem.isMagazineEmpty()
+        && !magazineSubsystem.isUpperBeamBroken()
+        && magazineSubsystem.getCurrUpperMagazineState() == UpperMagazineState.EMPTY;
   }
 
   @Override
   public void end(boolean interrupted) {
     turretSubsystem.stopTrackingTarget();
     shooterSubsystem.stop();
-    if (interrupted) {
-      magazineSubsystem.magazineInterrupted();
-    }
+    magazineSubsystem.magazineInterrupted();
   }
 }
