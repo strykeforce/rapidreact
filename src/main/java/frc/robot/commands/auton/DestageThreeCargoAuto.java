9648cade09744f3dbc574975bb0557632887ff1b
MHF3 <mfagan2006@gmail.com>
2022-07-26 21:08:09 -0400

    Added path to front of cargo

---
+++ src/main/java/frc/robot/commands/auton/DestageThreeCargoAuto.java
@@ -0,0 +1,66 @@
+package frc.robot.commands.auton;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.commands.drive.DriveAutonCommand;
+import frc.robot.commands.drive.OffsetGyroCommand;
+import frc.robot.commands.magazine.IgnoreColorSensorCommand;
+import frc.robot.commands.magazine.PreloadCargoCommand;
+import frc.robot.commands.sequences.intaking.AutoIntakeCommand;
+import frc.robot.commands.sequences.shooting.ArmShooterCommandGroup;
+import frc.robot.commands.sequences.shooting.VisionShootAutoCommand;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.IntakeExtendSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.MagazineSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.TurretSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
+
+public class DestageThreeCargoAuto extends SequentialCommandGroup {
+
+  public DestageThreeCargoAuto(
+      VisionSubsystem visionSubsystem,
+      TurretSubsystem turretSubsystem,
+      ShooterSubsystem shooterSubsystem,
+      MagazineSubsystem magazineSubsystem,
+      IntakeSubsystem intakeSubsystem,
+      IntakeExtendSubsystem intakeExtendSubsystem,
+      DriveSubsystem driveSubsystem,
+      String path1Name,
+      String path2Name,
+      String path3Name,
+      Rotation2d gyroOffset,
+      double delay,
+      double widthPixels) {
+    addCommands(
+        new ParallelCommandGroup(
+            new PreloadCargoCommand(magazineSubsystem),
+            new OffsetGyroCommand(driveSubsystem, gyroOffset)),
+        new WaitCommand(delay),
+        new ParallelDeadlineGroup(
+            new DriveAutonCommand(driveSubsystem, path1Name, true, true), // deadline
+            new ArmShooterCommandGroup(
+                visionSubsystem, turretSubsystem, shooterSubsystem, driveSubsystem),
+            new AutoIntakeCommand(
+                magazineSubsystem, intakeSubsystem, intakeExtendSubsystem, true, true)),
+        new VisionShootAutoCommand(
+            shooterSubsystem,
+            turretSubsystem,
+            magazineSubsystem,
+            visionSubsystem,
+            false,
+            intakeSubsystem,
+            widthPixels),
+        new IgnoreColorSensorCommand(magazineSubsystem, true),
+        new ParallelDeadlineGroup(
+            new DriveAutonCommand(driveSubsystem, path2Name, false, true),
+            new AutoIntakeCommand(
+                magazineSubsystem, intakeSubsystem, intakeExtendSubsystem, true, true)),
+        new WaitCommand(AutoConstants.kDefenseBallPickupDelay),
+        new DriveAutonCommand(driveSubsystem, path3Name, false, true));
+  }
+}
